ee73274345064e435daa1ce445f2deb04fff18e8
Joao Pedro Pio <joao.pio@agilecontent.com>
2018-02-17 18:18:41 -0200

    Added travis and docker

--- configs/app.js
+++ configs/app.js
@@ -1,6 +1,6 @@
 exports.port = process.env.APP_PORT || "2000";
 exports.domain = process.env.APP_DOMAIN || "localhost";
 
-const api_domain = process.env.API_DOMAIN || "localhost";
-const api_port = process.env.API_PORT || 5000;
+const api_domain = process.env.API_DOMAIN || "socialmovie.myddnsrocks";
+const api_port = process.env.API_PORT || "5000";
 exports.url = `http://${api_domain}:${api_port}`; 
